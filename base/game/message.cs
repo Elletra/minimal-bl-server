579f95d4626245fc2b908c99473b883f1a733c87
Elletra <elletra@proton.me>
2024-10-19 20:33:35 -0700

    Add more arguments to `messageClient()`

--- base/game/message.cs
+++ base/game/message.cs
@@ -1,4 +1,5 @@
-function messageClient(%client, %msgType, %msgString, %a1, %a2, %a3, %a4, %a5, %a6, %a7, %a8, %a9, %a10, %a11, %a12, %a13)
+// The number of arguments that can be passed into a function is hardcoded at 19.
+function messageClient(%client, %msgType, %msgString, %a1, %a2, %a3, %a4, %a5, %a6, %a7, %a8, %a9, %a10, %a11, %a12, %a13, %a14, %a15, %a16)
 {
-	commandToClient(%client, 'ServerMessage', %msgType, %msgString, %a1, %a2, %a3, %a4, %a5, %a6, %a7, %a8, %a9, %a10, %a11, %a12, %a13);
+	commandToClient(%client, 'ServerMessage', %msgType, %msgString, %a1, %a2, %a3, %a4, %a5, %a6, %a7, %a8, %a9, %a10, %a11, %a12, %a13, %a14, %a15, %a16);
 }
