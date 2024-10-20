ca47bb56dbbbd0bea19f4bd570c9c61d48d72e2b
Elletra <elletra@proton.me>
2024-10-19 20:34:21 -0700

    Automatically shut down server if server fails to be created

--- base/main.cs
+++ base/main.cs
@@ -23,6 +23,8 @@ function onStart()
 	if (!Server::create($Server::Type::Internet))
 	{
 		error("Failed to create server!");
+		schedule(5000, 0, echo, "Shutting down...");
+		schedule(6000, 0, quit);
 	}
 }
 
